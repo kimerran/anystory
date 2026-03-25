860f17455d100656153f5b651b24cad3b8c21d50
kimerran <mh.neri@gmail.com>
2026-03-25 09:46:21 +0800

    feat: replace emoji favicon with SVG book icon

--- app/icon.tsx
+++ app/icon.tsx
@@ -15,10 +15,21 @@ export default function Icon() {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          fontSize: 20,
         }}
       >
-        📖
+        <svg
+          width={20}
+          height={20}
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="white"
+          strokeWidth={1.5}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
+          <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
+        </svg>
       </div>
     ),
     { ...size }
